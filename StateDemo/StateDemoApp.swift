bae3274cc0a72856878e99753bd98e0ba4ecdaae
Dario Caric <dario.caric@gmail.com>
2023-12-16 23:55:21 +0100

    Initial Commit

---
+++ StateDemo/StateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateDemoApp.swift
+//  StateDemo
+//
+//  Created by Dario Caric on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct StateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
